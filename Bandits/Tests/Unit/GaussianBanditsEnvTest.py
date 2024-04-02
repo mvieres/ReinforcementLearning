9eeea36d58675302ded43f96279e1271b04385a2
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-04-02 22:31:30 +0200

    Unit test change

--- Bandits/Tests/Unit/GaussianBanditsEnvTest.py
+++ Bandits/Tests/Unit/GaussianBanditsEnvTest.py
@@ -2,14 +2,13 @@ import unittest
 
 from Bandits.Classes.GaussianBanditEnv import GaussianBanditEnv
 
-
 class GaussianEnvTest(unittest.TestCase):
 
     def test_step(self):
         action = [1, 2, 3, 4]
         mean_param = [0.3, 0.4, 0.5, 0.6]
         for i in range(len(action)):
-            reward = GaussianBanditEnv(mean_param=mean_param, max_steps=1).step(i)
+            reward = GaussianBanditEnv(mean_param=mean_param, max_steps=1, actions=[1, 2, 3, 4]).step(i)
             self.assertIsInstance(reward, float)
 
 
--- Bandits/Tests/Unit/UnitTests.py
+++ Bandits/Tests/Unit/UnitTests.py
@@ -4,13 +4,15 @@ from Bandits.Classes.Env import Env
 
 
 class EnvTest(unittest.TestCase):
-    def __int__(self):
-        self.test_float = [4, 4, 4, 4, 4]
+    def __int__(self, test_array):
+        self.test_float = test_array
+
     def test_q_value(self):
-        self.assertEqual(Env().q_value(rewards=self.test_float, return_std=False), 4)
+        self.assertEqual(Env().q_value(rewards=[4, 4, 4, 4], return_std=False), 4)
 
     def test_update_q_value(self):
         self.assertEqual(Env().update_q_value(reward_observation=4, q_old=4, n=2), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
